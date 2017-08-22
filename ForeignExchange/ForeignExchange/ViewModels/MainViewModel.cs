086c97ca384cb5e9d3adf2e4e87898cb662af9f2
Johan Gonzalez Posada <xjoau@hotmail.com>
2017-08-21 20:51:59 -0500

    Proceso final de clases Foreign exchange 1.0 con consumo de restfull usando picker

--- ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
+++ ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
@@ -3,9 +3,14 @@ namespace ForeignExchange.ViewModels
 {
     using ForeignExchange.Models;
     using GalaSoft.MvvmLight.Command;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.ComponentModel;
+    using System.Net.Http;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
     public class MainViewModel : INotifyPropertyChanged
     {
@@ -17,6 +22,8 @@ namespace ForeignExchange.ViewModels
 
         bool _isRunning;
         string _result;
+        bool _isEnable;
+        ObservableCollection<Rate> _rates;
 
 
         #endregion
@@ -30,8 +37,20 @@ namespace ForeignExchange.ViewModels
 
         public ObservableCollection<Rate> Rates
         {
-            get;
-            set;
+            get
+            {
+                return _rates;
+            }
+            set
+            {
+                if (_rates != value)
+                {
+                    _rates = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(Rates)));
+                }
+            }
         }
 
         public Rate SourceRate { get; set; }
@@ -57,7 +76,23 @@ namespace ForeignExchange.ViewModels
 
         }
 
-        public bool IsEnabled { get; set; }
+        public bool IsEnabled
+        {
+            get
+            {
+                return _isEnable;
+            }
+            set
+            {
+                if (_isEnable != value)
+                {
+                    _isEnable = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+            }
+        }
 
         public string Result
         {
@@ -89,12 +124,40 @@ namespace ForeignExchange.ViewModels
 
         #region Methonds
 
-        void LoadRates()
+        async void LoadRates()
         {
             IsRunning = true;
             Result = "Loading Rates...";
 
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("http://apiexchangerates.azurewebsites.net");
+                var controller = "api/Rates";
+                var response = await client.GetAsync(controller);
+                var result = await response.Content.ReadAsStringAsync();
 
+                if (!response.IsSuccessStatusCode)
+                {
+                    IsRunning = false;
+                    Result = result;
+                }
+
+                var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+                Rates = new ObservableCollection<Rate>(rates);
+
+                IsRunning = false;
+                Result = "Ready to convert!";
+
+                IsEnabled = true;
+
+            }
+            catch (Exception ex)
+            {
+
+                IsRunning = false;
+                Result = ex.Message;
+            }
 
         }
 
@@ -112,9 +175,56 @@ namespace ForeignExchange.ViewModels
             }
         }
 
-        void Convert()
+        async void Convert()
         {
-            
+            if (string.IsNullOrEmpty(Amount))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter in a value in amount!",
+                    "Accept");
+                return;
+            }
+
+            decimal amount = 0;
+
+            if (!decimal.TryParse(Amount, out amount))
+            {
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter in a numeric value in amount!",
+                    "Accept");
+                return;
+            }
+
+            if (SourceRate == null)
+            {
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a Source Rate!",
+                    "Accept");
+                return;
+            }
+
+            if (TargetRate == null)
+            {
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a Target Rate!",
+                    "Accept");
+                return;
+            }
+
+            var amountConverted = amount / (decimal)SourceRate.TaxRate * (decimal)TargetRate.TaxRate;
+
+            Result = string.Format("{0} {1:C2} = {2} {3:C2}", 
+                SourceRate.Code, 
+                amount, 
+                TargetRate.Code, 
+                amountConverted);
         }
 
         #endregion
